6ec15862431377b55ebf090a7bc2b729a7975c7a
D.S.Sathsarani <98662905+sathsaranigithub@users.noreply.github.com>
2024-12-19 21:12:35 +0530

    remove spaces
    remove unnecessary spaces

--- composeApp/src/androidMain/kotlin/org/example/cocoguard/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/cocoguard/MainActivity.kt
@@ -1,27 +1,18 @@
 package org.example.cocoguard
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-
 class MainActivity : ComponentActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             App()
         }
     }
 }
-
-
-
 @Preview
 @Composable
 fun AppAndroidPreview() {
--- composeApp/src/desktopMain/kotlin/org/example/cocoguard/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/cocoguard/main.kt
@@ -2,41 +2,23 @@ package org.example.cocoguard
 
 import com.google.firebase.FirebasePlatform
 import android.app.Application
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseOptions
-import dev.gitlive.firebase.firestore.firestore
 import dev.gitlive.firebase.initialize
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.security.MessageDigest
 
 fun main() = application {
-
     FirebasePlatform.initializeFirebasePlatform(object : FirebasePlatform() {
-
         val storage = mutableMapOf<String, String>()
         override fun clear(key: String) {
             storage.remove(key)
         }
-
         override fun log(msg: String) = println(msg)
-
         override fun retrieve(key: String) = storage[key]
-
         override fun store(key: String, value: String) = storage.set(key, value)
     })
-
     val options = FirebaseOptions(
         projectId = "coco-guard",
         applicationId = "1:858696594098:web:a8d46b5b27b74d8beccfab",
@@ -44,7 +26,6 @@ fun main() = application {
     )
 
     Firebase.initialize(Application(), options)
-
     Window(
         onCloseRequest = ::exitApplication,
         title = "Firestore",
@@ -54,10 +35,8 @@ fun main() = application {
             height = 720.dp
         )
     ) {
-
-            App()
-
-    }
+        App()
+      }
 }
 
 
